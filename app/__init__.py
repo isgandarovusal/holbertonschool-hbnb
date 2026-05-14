5142e679340e3527f6f7f40e34aa4b21c4211b1a
Sənin Adın <email@nmun.com>
2026-05-14 06:05:10 +0000

    Task 0: Implement Application Factory

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from config import config
+
+def create_app(config_name="default"):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    
+    # Extensions and Blueprints will be initialized here
+    
+    return app
