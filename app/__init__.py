d1bad96158211d6a7592446753ca495d9b33eba0
Vusal Isgandarov <isgandarovusal@gmail.com>
2026-05-14 06:11:10 +0000

    Task 5: Implement SQLAlchemy Repository and refactor Facade

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from config import config
 
+db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app(config_name="default"):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     
+    db.init_app(app)
     jwt.init_app(app)
     
-    # Blueprints initialization
     from .api.v1.auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
     
